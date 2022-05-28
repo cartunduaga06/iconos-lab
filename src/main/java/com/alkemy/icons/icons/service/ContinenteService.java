ba0d1c69a14373a9a64e7742d8dc72c4e41ac03a
cartunduaga06 <cartunduaga@gmail.com>
2022-05-27 23:57:02 -0500

    creando controller

---
+++ src/main/java/com/alkemy/icons/icons/service/ContinenteService.java
@@ -0,0 +1,18 @@
+package com.alkemy.icons.icons.service;
+
+import com.alkemy.icons.icons.dto.ContinenteDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContinenteService {
+
+    public ContinenteDTO save(ContinenteDTO dto) {
+        //guardar TODO:guardar continente
+
+
+        //devolver el continente guardad
+        System.out.println("guardar continente ");
+        return dto;
+
+    }
+}
